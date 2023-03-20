12c8acc32c7ec2e8400cff3f88ba8435d94b2545
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-20 21:07:55 +0900

    3/20 fontIcon

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Detail from './pages/Detail';
 import Product from './pages/Product';
 import Login from './pages/Login';
+import Navbar from './components/Navbar';
 
 
 // 쇼핑몰 프로젝트
 // 1. 전체상품페이지, 로그인, 상세상품페이지(페이지 3개)
+// 1-1. 네비게이션 바
 // 2. 로그인 버튼을 누르면 로그인 페이지가 나온다.
 // 3. 상품디테일을 눌렀으나, 로그인이 안되있을 경우에는 로그인 페이지가 나온다
 // 4. 로그인이 되어있을 경우 상품 디테일 페이지를 볼수 있다.
@@ -19,6 +21,7 @@ function App() {
 
   return (
     <div >
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Product/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
---
+++ shopping/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+
+    return ( 
+        <div>
+            <div>
+                <FontAwesomeIcon icon={faEnvelope} />
+            </div>
+
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Navbar;
