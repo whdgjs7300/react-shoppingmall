5a3f82d9bbad4853dedea210ce130e3b846c9cea
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-20 21:30:36 +0900

    3/20 navBar

--- shopping/src/components/Navbar.jsx
+++ shopping/src/components/Navbar.jsx
@@ -1,20 +1,46 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faSearch} from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
+    // Nav 리스트
+    const menuList = [  
+                        '여성', 
+                        'Divided',
+                        '남성',
+                        '신생아/유아',
+                        '아동',
+                        'H&M Home',
+                        'Sale',
+                        '지속가능성'
+                    ]
 
     return ( 
         <div>
-            <div>
-                <FontAwesomeIcon icon={faEnvelope} />
+            <div >
+                <div className='login-button'>
+                    <FontAwesomeIcon icon={faUser} />
+                    <div>로그인</div>
+                </div>
+            </div>
+                
+            <div className='nav-section'>
+                <img width={100} 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/1024px-H%26M-Logo.svg.png" alt="" />
             </div>
 
-            <div>
-
-            </div>
-
-            <div>
-
+            <div className='menu-area'>
+                    <ul className='menu-list'>
+                        {
+                            menuList.map((menu)=>(
+                                <li key={menu}>{menu}</li>
+                            ))
+                        }
+                    </ul>
+                <div className='search-box'>
+                        <FontAwesomeIcon icon={faSearch} />
+                        <input  type="text" placeholder='제품검색' />
+                </div>
             </div>
 
         </div>
