c371fb471d28438f135d9f7782609d15d8d96d56
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-21 17:56:15 +0900

    3/21 ProductGetAPI

---
+++ shopping/src/components/ProductCard.jsx
@@ -0,0 +1,18 @@
+
+
+const ProductCard = () => {
+
+
+
+    return ( 
+        <div>
+            <img src="https://lp2.hm.com/hmgoepprod?set=source[/ec/ef/ecef9d77c56c519e24a76b83331ae2c18f73f50d.jpg],origin[dam],category[],type[LOOKBOOK],res[y],hmver[1]&call=url[file:/product/main]" />
+            <div>Conscious Choice</div>
+            <div>벨티드 트윌 코트</div>
+            <div>가격</div>
+            <div>신제품</div>
+        </div>
+    );
+}
+
+export default ProductCard;
--- shopping/src/pages/Product.jsx
+++ shopping/src/pages/Product.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import ProductCard from "../components/ProductCard";
 
 
 const Product = () => {
+    // API 호출 후 데이터는 항상 State값에 넣어둠
+    const [productList,setProductList] = useState([]);
 
+    // Json-server에서 API 호출 함수
+    const getProducts = async() => {
+        let url = `http://localhost:5000/products`;
+        let response = await fetch(url);
+        let data = await response.json();
+        setProductList(data);
+    }
+
+    useEffect(()=>{
+        getProducts();
+    },[])
+    
     return ( 
         <div>
-            전체 상품 페이지
+            <ProductCard/>
         </div>
     );
 }
