06837f19a21805c0389ad4a666007cb46aca7d90
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-26 22:47:53 +0900

    3/26 DetailDataApi

--- shopping/src/pages/Detail.jsx
+++ shopping/src/pages/Detail.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Detail = () => {
+    const {id} = useParams();
+    const [product, setProduct] =useState(null);
+
+    const getProductDetail = async () => {
+        let url = `http://localhost:5000/products/${id}`;
+        let response = await fetch(url);
+        let data = await response.json();
+        setProduct(data);
+    }
+
+    // 상세 api 호출
+    useEffect(()=>{
+        getProductDetail()    
+    },[])
 
     return ( 
         <div>
-            상세페이지
+            
         </div>
     );
 }
