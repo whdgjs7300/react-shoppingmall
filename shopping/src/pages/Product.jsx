038d5269eb22d70b0bc2c226dbdde996e4cf14aa
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-27 16:07:08 +0900

    3/27 search

--- shopping/src/pages/Product.jsx
+++ shopping/src/pages/Product.jsx
@@ -2,15 +2,20 @@ import { useState } from "react";
 import { useEffect } from "react";
 import ProductCard from "../components/ProductCard";
 import {Col, Container, Row } from 'react-bootstrap';
+import { useSearchParams } from "react-router-dom";
 
 
 const Product = () => {
     // API 호출 후 데이터는 항상 State값에 넣어둠
     const [productList,setProductList] = useState([]);
+    const [query, setQuery] = useSearchParams();
 
     // Json-server에서 API 호출 함수
     const getProducts = async() => {
-        let url = `http://localhost:5000/products`;
+        let searchQuery = query.get('q')
+        // 이렇게 실행하면 위에 함수를 읽어올 수 없음 
+        // useEffect 두번째 인자값이 빈값이면 한번만 실행이됨
+        let url = `http://localhost:5000/products?q=${searchQuery}`;
         let response = await fetch(url);
         let data = await response.json();
         setProductList(data);
@@ -18,7 +23,7 @@ const Product = () => {
 
     useEffect(()=>{
         getProducts();
-    },[])
+    },[query])
     
     return ( 
         <div>
