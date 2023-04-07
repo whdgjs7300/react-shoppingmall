75e469bf5dc729b38e4b3122ae3d30680cdd0805
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-04-07 15:13:26 +0900

    4/7 devtoolsSet

--- shopping/src/redux/store.js
+++ shopping/src/redux/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware  } from 'redux'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/index';
 
 // 미들웨어 적용 
 // 첫번째 인자값인 리듀서가 여러개일 경우 합쳐줘야함
-let store = createStore(rootReducer, applyMiddleware(thunk));
+let store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
